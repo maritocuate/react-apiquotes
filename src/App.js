aea08ec9c455272dbbd79cbff1e4d39db065838c
marioquiroga <mquiroga@dosalcubo.com>
2020-06-16 19:37:42 -0300

    call api and set quote style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled'
+
+import Frase from './Frase'
 
 const Contenedor = styled.div`
   display: flex;
@@ -7,7 +9,6 @@ const Contenedor = styled.div`
   padding-top: 5rem;
   flex-direction: column;
 `;
-
 const Boton = styled.button`
   background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
   background-size: 300px;
@@ -17,19 +18,32 @@ const Boton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border:2px solid black;
+  transition: background-size .8s ease;
+
+  &:hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
+
+  const [quote, saveQuote] = useState({})
 
   const consultarApi = async () => {
     const consulta = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
     const respuesta = await consulta.json()
 
-    console.log(respuesta[0].quote)
+    saveQuote(respuesta[0])
   }
+
+  useEffect(()=>{
+    consultarApi()
+  }, [])
 
   return (
     <Contenedor>
+      <Frase frase={quote} />
       <Boton onClick={consultarApi}>Obtener frase</Boton>
     </Contenedor>
   );
